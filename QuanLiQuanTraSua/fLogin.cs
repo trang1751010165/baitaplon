92ab9812ef4484303e1def4aa937947dd529dd42
NguyenThiHaNi <ninguyenthiha99@gmail.com>
2019-10-25 21:23:32 +0700

    sua
    sua loi

--- QuanLiQuanTraSua/fLogin.cs
+++ QuanLiQuanTraSua/fLogin.cs
@@ -32,7 +32,7 @@ namespace QuanLiQuanTraSua
 
         private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if(MessageBox.Show("Bạn có thật sự muốn thoát chương trình?","thông báo",MessageBoxButtons.OKCancel)!=System.Windows.Forms.DialogResult.OK)
+            if(MessageBox.Show("Bạn có thật sự muốn thoát chương trình?","Thông báo",MessageBoxButtons.OKCancel)!=System.Windows.Forms.DialogResult.OK)
             {
                 e.Cancel = true;
             }
